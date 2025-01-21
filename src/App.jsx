b4825a508fa4b9cc932c9789483fa7cc367f9afa
SU-T <guitaristpeet@gmail.com>
2025-01-21 14:23:25 -0500

    created overall structure

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import apiClient from "./utils/apiClient";
 
 const App = () => {
   const initialLoad = async () => {
-    const response = await apiClient.get("/images/search?limit=10");
+    const response = await apiClient.get("/images/0XYvRd7oD");
     console.log(response.data);
   };
 
@@ -11,6 +11,24 @@ const App = () => {
     initialLoad();
   });
 
-  return <div>App</div>;
+  return (
+    <main>
+      <nav className="navbar">
+        <h1 className="logo">Purrtraits</h1>
+        <button className="btn">Change breeds</button>
+      </nav>
+      <div className="gallery">
+        <div>
+          <div className="column">
+            <img />
+          </div>
+        </div>
+        <div className="hero-text-container">
+          <h1 className="hero-text">Endless Cat</h1>
+          <h1 className="hero-text">Endless Joy</h1>
+        </div>
+      </div>
+    </main>
+  );
 };
 export default App;
